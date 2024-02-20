d5941db44004db75c2d7f3d507333ca15f1091d5
rsbastos <renedossantosbastos@gmail.com>
2024-02-20 12:25:49 -0300

    :sparkles: feat: sound implementation

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -28,6 +28,12 @@ function countDown() {
     }
 }
 
+function playSound (audioName) {
+let audio = new Audio(`./src/audios/${audioName}.m4a`);
+audio.volume = 0.2
+;audio.play();
+}
+
 function randomSquare() {
     state.view.squares.forEach((square) => {
         square.classList.remove("enemy");
@@ -46,6 +52,7 @@ function addListenerHitBox() {
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
+                playSound("hit");
             };
         });
     });
